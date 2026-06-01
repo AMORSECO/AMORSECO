fa12473f6b1367f3e1be67c9b466a66c033394ec
AMORSECO <elyeyo185@gmail.com>
2026-05-31 22:39:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-function changeTrail(name, color) {
-    const banner = document.getElementById('banner');
-    const trailText = document.getElementById('trail-name');
-    
-    // Cambia el color de fondo y el nombre del sendero
-    banner.style.backgroundColor = color;
-    trailText.innerText = name;
-}
+// La estructura es fija para reflejar la comparación simultánea.
+console.log("Estructura de senderos cargada");
